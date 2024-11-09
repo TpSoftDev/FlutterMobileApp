0cd1224a53a0367d885056fb04035b9d4ae2a5d9
Thabang <tp.softwaredev@gmail.com>
2024-11-09 12:28:05 -0600

    added a second button

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -29,54 +29,112 @@ class MyApp extends StatelessWidget {
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
 
+  //function for like button
+  void addLike() {}
   //function
   void getNext() {
     //new random word
     current = WordPair.random();
     notifyListeners(); // Notifies watchers & listeners
+  }
 
-    var favorites = <WordPair>[]; // stores fav words
+  var favorites = <WordPair>[]; // stores fav words
 
-    //logic for adding a current fav
-    void toggleFavorite() {
-      if (favorites.contains(current)) {
-        favorites.remove(current);
-      } else {
-        favorites.add(current);
-      }
-      notifyListeners(); // notify listeners
+  //logic for adding a current fav
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
     }
+    notifyListeners(); // notify listeners
   }
 }
 
+//
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current; // Good practice for efficiency
-
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BigCard(pair: pair),
-            SizedBox(height: 20),
-            // Add a button(widget)
-            ElevatedButton(
-              onPressed: () {
-                //Updating the apps state with the random word
-                appState.getNext();
+      body: Row(
+        children: [
+          //Adds padding at the top if needed (For Mobile phones)
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorites'),
+                ),
+              ],
+              selectedIndex: 0,
+              onDestinationSelected: (value) {
+                print('selected: $value');
               },
-              child: Text('Next'),
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: GeneratorPage(),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
 
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// ...
 class BigCard extends StatelessWidget {
   const BigCard({
     super.key,
