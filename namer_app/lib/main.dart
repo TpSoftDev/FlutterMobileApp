cb129617ee53cb469e770a57d82efbcf5c20e12d
Thabang <tp.softwaredev@gmail.com>
2024-11-09 00:44:16 -0600

    added a button and some UI to improve design

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -27,19 +27,66 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  //function
+  void getNext() {
+    //new random word
+    current = WordPair.random();
+    notifyListeners(); // Notifies watchers & listeners
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current; // Good practice for efficiency
 
     return Scaffold(
       body: Column(
         children: [
-          Text('A  random  idea:'),
-          Text(appState.current.asLowerCase),
+          Text('A  random awesome idea:'),
+          BigCard(pair: pair),
+
+          // Add a button(widget)
+          ElevatedButton(
+            onPressed: () {
+              //Updating the apps state with the random word
+              appState.getNext();
+            },
+            child: Text('Next'),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel:
+              pair.asPascalCase, //To help with accessability for screen readers
+        ),
       ),
     );
   }
